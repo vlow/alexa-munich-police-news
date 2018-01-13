21d0d07695d15b6c2705fca05f293a0c5ddf80af
Moritz Kammerer <kammerer.moritz@gmail.com>
2018-01-13 22:59:29 +0100

    Add HTML cleaning to all HTML text extraction

--- polizeiScraper/src/main/kotlin/de/turbocache3000/polizei/scraper/impl/NewsScraperImpl.kt
+++ polizeiScraper/src/main/kotlin/de/turbocache3000/polizei/scraper/impl/NewsScraperImpl.kt
@@ -6,6 +6,7 @@ import net.jodah.failsafe.Failsafe
 import net.jodah.failsafe.RetryPolicy
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.jsoup.safety.Whitelist
 import java.net.URI
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
@@ -66,17 +67,21 @@ class NewsScraperImpl(
         // Select tds which contains the news
         val elements = doc.select("td:has(${IndexSelectors.TITLE})")
         // Filter tds to only include the ones with a title matching the regexes
-        val newsElements = elements.filter { e -> allowedNewsTitles.any { it.matches(e.selectFirst(IndexSelectors.TITLE).text()) } }
+        val newsElements = elements.filter { e -> allowedNewsTitles.any { it.matches(clean(e.selectFirst(IndexSelectors.TITLE).text())) } }
 
 
         logger.debug("Index has {} news", newsElements.size)
         // Map from document to NewsIndex and NewsIndexEntry
         return NewsIndex(newsElements.map { e ->
-            val title = e.select(IndexSelectors.TITLE).text()
+            val title = clean(e.select(IndexSelectors.TITLE).text())
             val href = e.select(IndexSelectors.LINK).attr("href")
 
             NewsIndexEntry(title, baseURI.resolve(href))
         })
+    }
+
+    private fun clean(html: String): String {
+        return Jsoup.clean(html, Whitelist.none())
     }
 
     private fun fetchDocument(url: String): Document {
@@ -93,12 +98,12 @@ class NewsScraperImpl(
         logger.debug("Scraping news from {}", entry.uri)
         val doc = fetchDocument(entry.uri.toString())
 
-        val title = doc.select(NewsSelectors.TITLE).text()
+        val title = clean(doc.select(NewsSelectors.TITLE).text())
         logger.debug("Title: '{}'", title)
         val date = extractDate(title)
 
-        val entryTitles = doc.select(NewsSelectors.ENTRY_TITLE).map { it.text() }
-        val entryBodies = doc.select(NewsSelectors.ENTRY_BODY).map { it.text() }
+        val entryTitles = doc.select(NewsSelectors.ENTRY_TITLE).map { clean(it.text()) }
+        val entryBodies = doc.select(NewsSelectors.ENTRY_BODY).map { clean(it.text()) }
 
         logger.debug("Found {} entry titles and {} entry bodies", entryTitles.size, entryBodies.size)
         // Zip the entry titles and the entry bodies together to create the news entry
